33a7cbcf9e0491db0e56bca2171e7daf0eba7192
James Cuff <james_cuff@harvard.edu>
2018-02-13 17:31:43 -0500

    First import
    Initial import of client code

---
+++ README.md
@@ -0,0 +1,48 @@
+
+# Tablo Simple Client
+
+Simple Tablo client proof of concept to watch TV on an iPad on a treadmill with easy access to channel change buttons. 
+
+NOTE: THIS VERSION ONLY WORKS ON APPLE WEBKIT BROWSERS, SAFARI AND IOS
+      DUE TO THE USE OF M3U8 PLAYLISTS DELIVERED BY TABLO
+
+## Installing
+
+Set Tablo IP address in js/tablo.js
+
+````
+    var Tablo = "192.168.0.157";
+````
+
+Start a webserver:
+```
+[MacMini]$ python -m SimpleHTTPServer 8000
+
+```
+
+## Example:
+
+[![IMAGE ALT TEXT HERE](https://img.youtube.com/vi/NgzoxGgqtv4/0.jpg)](https://www.youtube.com/watch?v=NgzoxGgqtv4)
+
+
+## Packages used
+
+Kudos and thanks to each of the following that were used to build this app:
+
+* [Tablo Plex Channel](https://github.com/plexinc-plugins/Tablo.bundle)
+* [jQuery](http://jquery.com/download/)
+
+**NOTE:  All trademarks are of their respective owners.**  
+
+**This application is in NO WAY affiliated or endorsed with or by any product!**
+
+## Authors
+
+* **[James Cuff](http://twitter.com/jamesdotcuff)**
+* **[Michele Clamp](http://twitter.com/micheleclamp)**
+
+## TODO
+
+* Add channel change buttons
+* Grab thumbnails and "now playing guide data"
+* Probably 1,000 other ideas... :-)
---
+++ js/tablo.js
@@ -0,0 +1,47 @@
+$(function(){
+
+    // Set IP of Tablo Device here, this is the only config.
+
+    var Tablo = "192.168.0.157";
+    
+    $.get("http://" + Tablo + ":8885/guide/channels", function(channels = JSON.parse(data)){
+     var index; 
+     for (index = 0; index < channels.length; ++index) {
+         ThisChannel = channels[index].split("/");
+         tmpurl = "http://192.168.0.157:8885/guide/channels/" + ThisChannel[3]; 
+         watchurl = "http://" + Tablo + ":8885/guide/channels/" + ThisChannel[3] + "/watch";
+         watchstr = '<a href="' + watchurl + '">watch</a>';
+         callsigns = {};
+         getCallSign(tmpurl, ThisChannel[3]);
+     }
+  });
+});
+
+function getVideoURL(chanid,callsign){
+   tmpurl = "http://" + Tablo +":8885/guide/channels/" + chanid + "/watch";
+   document.getElementById("status").innerHTML =  "Tuning to... " + callsign + " please wait...";
+   console.log("Tuning to... " + callsign + "please wait...");
+   $.post(tmpurl,function(watch = JSON.parse(data)) {
+      console.log(watch.playlist_url);
+      document.getElementById("Video1").innerHTML = "\n\nWatch URL\n" + watch.playlist_url + "\n\n";
+      document.getElementById("Video1").innerHTML = '<video height="400" width="720" playsinline autoplay controls src="' + watch.playlist_url + '" type="application/x-mpegurl" class="media-document mac video"></video>';
+      document.getElementById("status").innerHTML =  "Now playing " + callsign;
+    });
+}
+
+function getCallSign(tmpurl,chanid){
+  $.get(tmpurl,function(data) {
+     result = data.channel.major + "."
+       + data.channel.minor
+       + " " + data.channel.resolution
+       + "\n";
+     callsigns[data.channel.call_sign] = result;
+     buttonid = "channel_" + data.object_id;
+     console.log(buttonid);
+     $('#channel_table tr:last').after("<tr><td><input id='" + buttonid + "' class='button' type='button' value='" + data.channel.call_sign +  "'/></td></tr>");
+     $('#'+buttonid).on('click',function() {
+          getVideoURL(chanid,data.channel.call_sign);
+     });
+  });
+}
+
